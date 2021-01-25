7adaf9378411edd766111b4044cbff7c4f143683
jd812815 <77907449+jd812815@users.noreply.github.com>
2021-01-25 08:34:27 -0500

    Update bookmarks.sh

--- bookmarks.sh
+++ bookmarks.sh
@@ -3,3 +3,11 @@ https://www.digitalocean.com/community/tutorials/how-to-add-and-delete-users-on-
 
 mail:
 https://www.linuxtechi.com/install-configure-postfix-mailserver-centos-8/
+
+
+XMPP:
+https://docs.ejabberd.im/admin/installation/#install-on-linux
+https://www.ejabberd.im/forum/28654/user-registration/index.html
+
+TOR:
+https://medium.com/@ajphillips/how-to-create-your-own-tor-hidden-service-436bece8602f
