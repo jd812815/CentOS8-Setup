aea989ca2ab23686fcbbb69d67a3c0baaf78c37a
jd812815 <77907449+jd812815@users.noreply.github.com>
2021-01-25 08:30:24 -0500

    Update bookmarks.sh

--- bookmarks.sh
+++ bookmarks.sh
@@ -1,2 +1,5 @@
 Users:
 https://www.digitalocean.com/community/tutorials/how-to-add-and-delete-users-on-a-centos-7-server
+
+mail:
+https://www.linuxtechi.com/install-configure-postfix-mailserver-centos-8/
